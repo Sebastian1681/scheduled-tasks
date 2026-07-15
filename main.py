e1657c7d32d7cc98a1da55fdd13814a3abe15fbf
Sebastian1681 <sebastian.olteanu81@gmail.com>
2026-07-15 15:22:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,73 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
 import pandas
+import datetime as dt
 import random
 import smtplib
-import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+now = dt.datetime.now()
+today = (now.month, now.day)
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+# print(today)
+# 2. Check if today matches a birthday in the birthdays.csv
+# HINT 1: Create a tuple from today's month and day using datetime. e.g.
 
+# HINT 2: Use pandas to read the birthdays.csv
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+# print(data)
+# HINT 3: Use dictionary comprehension to create a dictionary from birthday.csv that is formated like this:
+birthdays_dict = {
+    (data_row["month"], data_row["day"]): data_row
+    for index, data_row in data.iterrows()
+}
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+
+    random_letter_number = random.randint(1, 3)
+
+    with open(f"letter_templates/letter_{random_letter_number}.txt") as letter_file:
+        letter_contents = letter_file.read()
+
+    personalized_letter = letter_contents.replace("[NAME]", birthday_person["name"])
+
+    my_email = "sebastian.olteanu81@gmail.com"
+    password = "hvgzanalrdvfmiba"
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.login(user=my_email, password=password)
         connection.sendmail(
-            from_addr=MY_EMAIL,
+            from_addr=my_email,
             to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+            msg=f"Subject:Happy Birthday!\n\n{personalized_letter}"
         )
+
+# birthday_person = birthdays_dict[today]
+# print(birthday_person)
+# print(birthdays_dict)
+#Dictionary comprehension template for pandas DataFrame looks like this:
+# new_dict = {new_key: new_value for (index, data_row) in data.iterrows()}
+#e.g. if the birthdays.csv looked like this:
+# name,email,year,month,day
+# Angela,angela@email.com,1995,12,24
+#Then the birthdays_dict should look like this:
+# birthdays_dict = {
+#     (12, 24): Angela,angela@email.com,1995,12,24
+# }
+
+#HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
+# if (today_month, today_day) in birthdays_dict:
+
+# 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
+# HINT 1: Think about the relative file path to open each letter. 
+# HINT 2: Use the random module to get a number between 1-3 to pick a randome letter.
+# HINT 3: Use the replace() method to replace [NAME] with the actual name. https://www.w3schools.com/python/ref_string_replace.asp
+
+# 4. Send the letter generated in step 3 to that person's email address.
+# HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+# HINT 2: Remember to call .starttls()
+# HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
+# HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
+
+
+
